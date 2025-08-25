c6fcd342c340e2c87ad5317bfe2c9c02d07644f8
yana-bhuyan <yanabhuyan2006@gmail.com>
2025-08-25 11:25:55 +0000

    experiment_5_shell

--- hello.sh
+++ hello.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
+# This is a simple shell cript
 
-echo "Enter a number:"
-read num
- 
-if [ $num -gt 10 ]
-then
- echo "The number $num is greater than 10"
-
-else 
- echo "The number $num is  10 or smaller"
-
-fi
+echo "Hello, World!"
